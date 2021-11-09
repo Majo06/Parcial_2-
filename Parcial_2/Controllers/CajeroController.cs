c9d080ad17b021e99495dc667f89c70df3e322a9
Majo06 <am0675032021@unab.edu.sv>
2021-11-09 01:35:20 -0600

    Cajero

---
+++ Parcial_2/Controllers/CajeroController.cs
@@ -0,0 +1,40 @@
+﻿using Parcial_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parcial_2.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Correcto()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult incorrecto()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult index(Cajero c)
+        {
+            if (c.Monto % 5 == 0)
+            {
+                return Redirect("Correcto");
+            }
+            else
+            {
+                return Redirect("incorrecto");
+            }
+        }
+    }
+}
---
+++ Parcial_2/Models/Cajero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial_2.Models
+{
+    public class Cajero
+    {
+        public int Monto { get; set; }
+
+    }
+}
